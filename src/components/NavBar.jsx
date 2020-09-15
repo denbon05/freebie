bc26bd551b5e1e624cc0d774a79abd617cebf770
den <denisamigous@gmail.com>
2020-09-15 23:49:42 +0200

    example 1 with React-bootstrap

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { Navbar, Nav, Form, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import cn from 'classnames';
+import FontAwesome from 'react-fontawesome';
 import * as actions from '../actions/index.js';
 import '../css/NavBar.css';
+// import '../css/custom.css';
 import logo from '../images/logo.png';
 
 const mapStateToProps = (state) => {
@@ -40,9 +42,9 @@ class NavBar extends React.Component {
   render() {
 
     return (
-      <div className="bgHeader">
-        <Navbar>
-          <Navbar.Brand href="#home">
+      <div className="bgHeader container">
+        <Navbar className="row">
+          <Navbar.Brand href="#home" className="col-4">
             <img
               src={logo}
               width="133"
@@ -51,7 +53,7 @@ class NavBar extends React.Component {
               alt="React Bootstrap logo"
             />
           </Navbar.Brand>
-          <Navbar.Collapse id="responsive-navbar-nav">
+          <Navbar.Collapse id="responsive-navbar-nav" className="col-8">
             <Nav>
               {this.renderLinkNav()}
             </Nav>
@@ -59,19 +61,30 @@ class NavBar extends React.Component {
         </Navbar>
         
         <div id="allSearchForm">
-          <div className="containerForm">
-            <h1 id="hSearchForm">I'm looking for..</h1>
-            <Form id="searchForm">
-              <Form.Control type="search" placeholder="Enter a Job Description" />
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button id="buttonSearch" variant="primary" type="submit">
-                Submit
+          <div className="containerForm container">            
+            <Form id="searchForm" className="row">
+              <h1 id="hSearchForm" className="col-8">I'm looking for..</h1>  
+              <Form.Control type="search" id="searchInput" size="lg" placeholder="Enter a Job Description" />
+              <Button variant="info" id="buttonSearch" type="submit">
+                <FontAwesome
+                  className='super-crazy-colors'
+                  name='search'
+                  style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+                />{' '}
+                  Search
               </Button>
+              <div key={`inline-checkbox`} className="mb-3">
+                <Form.Check inline type='checkbox' className='inlineCheckbox1' label="Full Time" />
+                <Form.Check inline type='checkbox' className='inlineCheckbox2' label="Part Time / Casual / Freelance" />
+                <Form.Group controlId="formGridState">
+                <Form.Control size="sm" as="select" defaultValue="Choose...">
+                  <option>Choose...</option>
+                  <option>...</option>
+                </Form.Control>
+              </Form.Group>
+              </div>
+              
+              
             </Form>
           </div>
         </div>
